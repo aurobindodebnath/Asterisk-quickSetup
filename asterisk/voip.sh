8012b81eb1986441dac9d2fc60340feb31028849
Aurobindo Debnath <aurobindodebnath@gmail.com>
2019-04-20 08:09:01 -0400

    Minor path changes

--- asterisk/voip.sh
+++ asterisk/voip.sh
@@ -16,19 +16,19 @@ make
 make install
 make samples
 cd ~
-cp ~quickSetup/asterisk/init.d/asterisk /etc/init.d/asterisk
+cp ~/quickSetup/asterisk/init.d/asterisk /etc/init.d/asterisk
 
 useradd -d /var/lib/asterisk asterisk
 #add password keystrokes if necessary
 chown -R asterisk /var/spool/asterisk /var/lib/asterisk /var/run/asterisk
-cp ~quickSetup/asterisk/default/asterisk /etc/default/asterisk
+cp ~/quickSetup/asterisk/default/asterisk /etc/default/asterisk
 update-rc.d asterisk default
 
 echo "Setting up config files"
 mv /etc/asterisk/sip.conf /etc/asterisk/sip.conf.orig
 mv /etc/asterisk/extensions.conf /etc/asterisk/extensions.conf.orig
-cp ~quickSetup/asterisk/sip.conf /etc/asterisk/sip.conf
-cp ~quickSetup/asterisk/extensions.conf /etc/asterisk/extensions.conf
+cp ~/quickSetup/asterisk/sip.conf /etc/asterisk/sip.conf
+cp ~/quickSetup/asterisk/extensions.conf /etc/asterisk/extensions.conf
 
 echo "Lets run Asterisk"
 /etc/init.d/asterisk start
